e21dbc3a15f8f7a1bc976b3dd0ae2c7c9b275c8a
Lozañes, Xyrus Jehan B <78250667+XyrusJehan@users.noreply.github.com>
2025-06-25 09:48:34 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $host = 'ballast.proxy.rlwy.net'; // or 'ballast.proxy.rlwy.net' for public URL
 $dbname = 'railway';
 $username = 'root';
 $password = 'mfwZMSewsBKfBJQOdeOmyqMZoRGwewMI';
-$port = 3306; // or 33262 if using the public URL
+$port = 33262; // or 33262 if using the public URL
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
