53db7da5c300aaf50c0c470f257481789d6b783b
Lozañes, Xyrus Jehan B <78250667+XyrusJehan@users.noreply.github.com>
2025-06-25 09:09:29 +0800

    Add files via upload

--- auth/google/login.php
+++ auth/google/login.php
@@ -1,34 +1,21 @@
 <?php
 require_once __DIR__.'/../../config/google_oauth.php';
-require_once __DIR__.'/../../includes/security.php';
 
 session_start();
-
-// Generate CSRF token
-$csrfToken = bin2hex(random_bytes(32));
-$_SESSION['oauth_csrf_token'] = $csrfToken;
 
 $googleConfig = require __DIR__.'/../../config/google_oauth.php';
 
 $params = [
     'response_type' => 'code',
     'client_id' => $googleConfig['client_id'],
-    'redirect_uri' => 'https://learnmate.up.railway.app/auth/google/callback',
+    'redirect_uri' => $googleConfig['redirect_uri'],
     'scope' => implode(' ', $googleConfig['scopes']),
-    'state' => generateStateToken(),
-    'nonce' => bin2hex(random_bytes(16)),
+    'access_type' => 'online',
     'prompt' => 'select_account',
-    'access_type' => 'online'
+    'state' => bin2hex(random_bytes(16))
 ];
 
-// Store all security tokens
 $_SESSION['oauth_state'] = $params['state'];
-$_SESSION['oauth_nonce'] = $params['nonce'];
-
 header('Location: ' . $googleConfig['authorization_endpoint'] . '?' . http_build_query($params));
 exit;
-
-function generateStateToken() {
-    return hash_hmac('sha256', bin2hex(random_bytes(32)), 'your_secret_key_here');
-}
 ?>
